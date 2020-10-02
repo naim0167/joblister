267e51b422fb9e9241beec023b713d861d0da730
Md. Naim Chowdhury <naimmcduck@gmail.com>
2020-10-02 06:25:10 +0200

    template joblister

---
+++ lib/Template.php
@@ -0,0 +1,28 @@
+<?php
+
+class Template{
+    //path
+    protected $template;
+    //vars Passed In
+    protected $vars = array();
+
+    //contructor
+    public function __construct($template){
+        $this->template= $template;
+    }
+    public function __get($key){
+        return $this->vars[$key];
+    }
+    public function __set($key, $value){
+        return $this->vars[$key] = $value;
+    }
+    public function __toString(){
+        extract($this->vars);
+        chdir(dirname($this->template));
+        ob_start();
+
+        include basename($this->template);
+
+        return ob_get_clean();
+    }
+}
